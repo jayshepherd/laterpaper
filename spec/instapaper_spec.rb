36f8451f6343b888707a4d29444f0805fd76d6b4
Jay Shepherd <jaystuff@shepjour.com>
2011-01-13 03:03:06 -0600

    refactored tests

--- spec/instapaper_spec.rb
+++ spec/instapaper_spec.rb
@@ -1,44 +1,48 @@
 require 'instapaper'
 
 describe Instapaper do
-  it "should return true when checking the authentication of a user with valid credentials" do
-    username = 'jaystuff@shepjour.com'
-    password = 'password'
 
-    ip = Instapaper.new(username, password)
-    ip.check_auth?.should == true
-  end
-  
-  it "should return false when checking the authentication of a user with invalid credentials" do
-    username = 'jaystuff@shepjour.com'
-    password = 'bogus'
-    
-    ip = Instapaper.new(username, password)
-    ip.check_auth?.should == false
+  context "with invalid username or password" do
+    before do
+      @ip = Instapaper.new('jaystuff@shepjour.com','bogus')
+    end
 
-  end
-  
-  it "should return a code of 403 when checking the auth of a user with invalid credentials" do
-    username = 'jaystuff@shepjour.com'
-    password = 'bogus'
-
-    ip = Instapaper.new(username, password)
-    if ip.check_auth? == false
-      ip.return_code.should == "403"
+    it "should return false when checking the credentials" do
+      @ip.check_auth?.should == false
+      @ip.return_code.should == "403"
     end
   end
-
-  it "should return a code of 500 when there is a server problem"
-
-  context "when a valid username and password" do
+  
+  context "with a valid username and password" do
     before do
       @ip = Instapaper.new('jaystuff@shepjour.com','password')
     end
-    it "should add a url" do
+  
+    it "should return true when checking the credentials" do
+      @ip.check_auth?.should == true
+    end
+    
+    it "should succeeed on a valid url" do
       @ip.add?("www.sprucecreeksoftware.com").should == true
-    end  
+    end 
+
+    it "should fail with a 400 on an invalid url" do
+      @ip.add?("http://www.foobar.com").should == false
+      @ip.return_code.should == "404"
+    end
+
+    it "should fail with a 403 on an invalid username and password"
+
+    it "should fail with a 500 if there is a server problem"
+
+    it "should fail with a 400 if there is no url provided" do
+      @ip.add?("").should == false
+      @ip.return_code.should == "400"
+    end
+
   end
 
+  it "should return a code of 500 when there is a server problem"
 
 
 end
